3a5e766c7c8d16f528724a59ecafc368416d8d40
mukul-sharma085 <smukul085@gmail.com>
2025-10-15 23:45:19 +0530

    ui

--- src/app/teacher/dashboard/notes/page.tsx
+++ src/app/teacher/dashboard/notes/page.tsx
@@ -1,30 +1,14 @@
 "use client"
 
-import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable"
-import { PlateEditor } from '@/components/editor/plate-editor';
-import { PlateController } from 'platejs/react';
-import { TocSideBar } from '@/components/ui/toc-sidebar';
-import  NotesListingPage  from "@/features/notes/notes-listing";
+import  NotesPage  from "@/features/notes/notes-page";
 
 
-
-export default function NotesMakerPage() {
+export default function Page() {
 
 
   return (
-    <PlateController>
-    <section className="h-[calc(100vh-6rem)]">
-      <NotesListingPage />
-      <ResizablePanelGroup direction="horizontal" className="h-full rounded-md border">
-        <ResizablePanel defaultSize={15} minSize={10} className="bg-muted/30">
-          <TocSideBar />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={85} minSize={85} >
-          <PlateEditor />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </section>
-  </PlateController>
+   <>
+   <NotesPage />
+   </>
   )
 }
---
+++ src/features/notes/create-notes.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable"
+import { PlateEditor } from '@/components/editor/plate-editor';
+import { PlateController } from 'platejs/react';
+import { TocSideBar } from '@/components/ui/toc-sidebar';
+
+
+
+export default function CreateNotes() {
+
+
+  return (
+    <PlateController>
+    <section className="h-[calc(100vh-6rem)]">
+      <ResizablePanelGroup direction="horizontal" className="h-full rounded-md border">
+        <ResizablePanel defaultSize={15} minSize={10} className="bg-muted/30">
+          <TocSideBar />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={85} minSize={85} >
+          <PlateEditor />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </section>
+  </PlateController>
+  )
+}
