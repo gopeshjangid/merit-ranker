0f819e76df699e2e94b29a0a8cc8dce7be054824
mukul-sharma085 <smukul085@gmail.com>
2025-10-16 10:51:16 +0530

    presentation layout ui

--- src/app/teacher/dashboard/live-class/presentation/layout.tsx
+++ src/app/teacher/dashboard/live-class/presentation/layout.tsx
@@ -10,10 +10,10 @@ export default function PresentationLayout({
   return (
     <>
       <PresentationGenerationManager />
-      <div className="flex h-full w-full flex-col supports-[(height:100dvh)]:h-[100dvh]">
+      <div className="flex h-full w-full flex-col">
         <PresentationHeader />
-        <main className="relative flex flex-1 overflow-hidden">
-          <div className="sheet-container h-[calc(100vh-3.8rem)] flex-1 place-items-center overflow-x-clip overflow-y-auto supports-[(height:100dvh)]:h-[calc(100dvh-3.8rem)]">
+        <main className="relative flex flex-1">
+          <div className="flex-1 place-items-center">
             {children}
           </div>
         </main>
