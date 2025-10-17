be8b56a0b1a2cb8b51e1906dcc92a8a6543c858a
mukul-sharma085 <smukul085@gmail.com>
2025-10-17 14:50:54 +0530

    config

--- src/components/landing/footer-wrapper.tsx
+++ src/components/landing/footer-wrapper.tsx
@@ -5,7 +5,7 @@ import { Footer } from "./footer";
 
 export function FooterWrapper() {
   const pathname = usePathname();
-  const excludedPrefixes = ["/teacher"]; // Customize as needed
+  const excludedPrefixes = ["/teacher","/login"]; // Customize as needed
   const shouldShowFooter = !excludedPrefixes.some(prefix => pathname.startsWith(prefix));
 
   return shouldShowFooter ? <Footer /> : null;
